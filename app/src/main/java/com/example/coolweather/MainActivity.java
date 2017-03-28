422930f6b9325b0eadc01d9d86cd0bcf61bb4b8d
Zhao <597611084@qq.com>
2017-03-28 11:45:59 +0800

    First commit.

---
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//和风天气个人认证key：5ec46b787c1c43c0b72c6e9b2402dac3
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
